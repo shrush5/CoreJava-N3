1276ca2f3a6f024791b52e4ecea74fdcdde63a0a
shrushti ugale <shrushtiugale1@gmail.com>
2023-02-01 20:34:35 +0530

    first commit

---
+++ dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,11 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to first java program");
+		// TODO Auto-generated method stub
+
+	}
+
+}

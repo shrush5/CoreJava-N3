1537b90f9f484f01ad90bff443b46ef164f42ca6
shrushti ugale <shrushtiugale1@gmail.com>
2023-02-02 13:06:01 +0530

    second commit

--- dayone/src/org/tnsif/intro/DatatypeDemo.java
+++ dayone/src/org/tnsif/intro/DatatypeDemo.java
@@ -1,26 +1,36 @@
 package org.tnsif.intro;
 
 public class DatatypeDemo {
-    //to demostrate the program on data type 
+    
+	//to demostrate the program on data type 
 	public static void main(String[] args) {
+		
 		//byte 1 byte
 		byte a =127;
+		
 		//int 2 byte
 		int b =128;
+		
 		//float 4 byte
 		float c =12/5;
 		float d = 5/2.5f;
+		
 		//double 8 byte
 		double e = 12/5;
 		double f = 12d/5d;
+		
 		//character
 		char z = 's';
+		
 		// ascii rep
 		int x = '1';
+		
 		//boolean
 		boolean h = true;
+		
 		//string
 		String name = "Shrushti";
+		
 		System.out.println("The value of a is : "+a);
 		System.out.println("The value of b is : "+b);
 		System.out.println("The value of c is : "+c);
---
+++ dayone/src/org/tnsif/intro/UserInputDemo.java
@@ -0,0 +1,38 @@
+package org.tnsif.intro;
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		// User input
+		
+		Scanner  sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		System.out.println("Enter the value of int");
+
+		float y =sc.nextFloat();
+		System.out.println("Enter the value of float");
+
+		char z = sc.next().charAt(0);
+		System.out.println("Enter the value of char");
+
+		String name=sc.next();
+		System.out.println("Enter the value of name");
+
+		String str=sc.nextLine();
+		System.out.println("Enter the value of str");
+
+		double d=sc.nextDouble();
+		System.out.println("Enter the value of double");
+		
+		System.out.println("The value of x is:" +x);
+		System.out.println("The value of y is:" +y);
+		System.out.println("The value of z is:" +z);
+		System.out.println("The value of name is:" +name);
+		System.out.println("The value of str is:" +str);
+		System.out.println("The value of d is:" +d);
+
+	}
+
+}

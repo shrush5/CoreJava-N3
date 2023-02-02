7ad10ed7afd8be7e955caab4d7675f321742ce52
shrushti ugale <shrushtiugale1@gmail.com>
2023-02-02 11:50:53 +0530

    second commit

---
+++ dayone/src/org/tnsif/intro/DatatypeDemo.java
@@ -0,0 +1,30 @@
+package org.tnsif.intro;
+
+public class DatatypeDemo {
+    //to demostrate the program on data type 
+	public static void main(String[] args) {
+		//byte 1 byte
+		byte a =127;
+		//int 2 byte
+		int b =128;
+		//float 4 byte
+		float c =12/5;
+		float d = 5/2.5f;
+		//double 8 byte
+		double e = 12/5;
+		double f = 12d/5d;
+		//character
+		char z = 's';
+		System.out.println("The value of a is : "+a);
+		System.out.println("The value of b is : "+b);
+		System.out.println("The value of c is : "+c);
+		System.out.println("The value of d is : "+d);
+		System.out.println("The value of e is : "+e);
+		System.out.println("The value of f is : "+f);
+		System.out.println("The value of z is : "+z);
+		
+	
+
+	}
+
+}

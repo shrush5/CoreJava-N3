1bd2535031a262abea24359460821677f2bb295f
shrushti ugale <shrushtiugale1@gmail.com>
2023-02-21 06:43:12 +0530

    first commit

---
+++ dayeleven/src/org/tnsif/annotations/OverrideAnnotatioDemo.java
@@ -0,0 +1,27 @@
+package org.tnsif.annotations;
+
+//demo on Override annotation
+class Parent{
+	public void print(){
+		System.out.println("Parent class method");
+	}
+}
+
+class Child extends Parent{
+	@Override
+	
+	public void print(){
+		super.print();
+		System.out.println("Child class method");
+	}
+	
+}
+public class OverrideAnnotatioDemo {
+
+	public static void main(String[] args) {
+		Parent c=new Child();
+		c.print();
+
+	}
+
+}
